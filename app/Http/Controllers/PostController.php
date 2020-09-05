812f16e459f92eb45156730536c52b97de747152
giannurwana19 <giannurwana19@gmail.com>
2020-09-05 22:24:19 +0700

    belajar eloquent #11

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        // k: jika post nya kosong
+        // if(!$post){ // atau is_null($post) 
+        //     abort(404); // redirect ke page 404
+        // }
+
         return view('posts.show', compact('post'));
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,4 +2,5 @@
 @section('title', $post->title)
 @section('content')
   <h1>{{ $post->title }}</h1>
+  <p>{{ $post->body }}</p>
 @endsection
