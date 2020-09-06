70ebaa316c405f96c468026b68339c949af6dd9c
giannurwana19 <giannurwana19@gmail.com>
2020-09-06 10:58:09 +0700

    bkerja dengan tinker pada factory #16

--- app/Post.php
+++ app/Post.php
@@ -2,12 +2,21 @@
 
 namespace App;
 
+use GuzzleHttp\Psr7\FnStream;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    // public function getRouteKeyName()
-    // {
-    //     return 'slug';
-    // }
+
+    // h: etika buat funtion di model, itu dibaca per item, bukan keseluruhan dari model itu
+    // solusinya guakan keyword scope 
+    public function scopeLatestFirst()
+    {
+        return $this->latest()->first();
+    }
+
+    public function scopeLatestPost()
+    {
+        return $this->latest()->get();
+    }
 }
