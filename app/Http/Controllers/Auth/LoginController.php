867d373fdcd9a7810dcdcb052d60a00fef4b7d55
giannurwana19 <giannurwana19@gmail.com>
2020-09-08 21:02:02 +0700

    mengubah default login dengan email menjadi username #34

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // ganti login yang sebelumnya dgn email menjadi username
+    public function username()
+    {
+        return 'username';
+    }
 }
